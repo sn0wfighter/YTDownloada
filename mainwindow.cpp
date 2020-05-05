53d15ab7d7f291273aa70a6d433abe1763aafeed
sn0wfighter <snowfighter@mailbox.org>
2020-05-05 08:37:12 +0200

    include new icon

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,7 @@
 #include "ui_mainwindow.h"
 #include <qprocess.h>
 #include "mydialog.h"
-#include "downloadentry.h"
+//#include "downloadentry.h"
 #include <qdir.h>
 #include <QDebug>
 #include <QCoreApplication>
